c80aadc7e667aadba75672924d27ada6b4f22ec6
eastandwestservices <your_email@example.com>
2025-12-16 02:38:49 +0500

    Update Astro config and add Tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://dprobo.com',
-  output: 'static',
+  site: 'https://biobolics-astro.vercel.app', // your Vercel domain
+  output: 'static',                             // must be 'static'
   integrations: [tailwind(), sitemap()],
 });
